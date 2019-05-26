7b4f0b5c454929d240a065b1c3de25225cd8121f
hope.hy.cn <hope.hy.cn@gmail.com>
2019-05-26 23:34:37 +0800

    demo

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+  mongoURI:
+    'mongodb://nodevueelement:test1234@ds223756.mlab.com:23756/node-vue-element',
+  secretOrKey: 'secret'
+};
+// mongodb+srv://test:<password>@cluster0-xt7ra.mongodb.net/test?retryWrites=true
